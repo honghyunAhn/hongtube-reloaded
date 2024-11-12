0ce592dad1778dcf4b9982c2cf7e2f3b2577a967
Ahn Honghyun <anh2073@naver.com>
2024-11-12 18:46:22 +0900

    Get Requests

--- src/server.js
+++ src/server.js
@@ -4,7 +4,11 @@ const PORT = 4000;
 
 const app = express();
 
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log(`✅Server listenting on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
