3a9a43b65fa085ece2563768a88d2f5b15ef6ada
Ahn Honghyun <anh2073@naver.com>
2024-11-19 16:44:13 +0900

    Recap

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I love you.");
+  return res.send("<h1>I love you.</h1>");
 };
 const handleLogin = (req, res) => {
-  return res.send("Login here.");
+  return res.send({ message: "Login here." });
 };
 app.get("/", handleHome);
 app.get("/login", handleLogin);
