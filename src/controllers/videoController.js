6a6b3727805fd8731623fe7e02237306cfa26d58
Ahn Honghyun <anh2073@naver.com>
2024-11-21 17:06:56 +0900

    Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -3,6 +3,4 @@ export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
-export const deleteVideo = (req, res) => {
-  res.send("Delete Viedo");
-};
+export const deleteVideo = (req, res) => res.send("Delete Viedo");
