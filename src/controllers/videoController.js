cd87316e309336c6d317a99dd18c42a6278d6904
Ahn Honghyun <anh2073@naver.com>
2024-11-21 18:31:14 +0900

    Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,7 @@
-const fakeUser = {
-  username: "anh288",
-  loggedIn: true,
+export const trending = (req, res) => {
+  const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return res.render("home", { pageTitle: "home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "home", fakeUser: fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
 export const search = (req, res) => res.send("Search");
