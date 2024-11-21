46077c83f38182597fccb589990c5da71559b6d0
Ahn Honghyun <anh2073@naver.com>
2024-11-21 14:49:15 +0900

    Exports

--- src/server.js
+++ src/server.js
@@ -11,8 +11,8 @@ const logger = morgan("dev");
 app.use(logger);
 
 app.use("/", globalRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
