5b55b55123888078552c1d13e01897dc12e5801b
Ahn Honghyun <anh2073@naver.com>
2024-11-25 10:15:39 +0900

    Edit Video part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -32,7 +32,13 @@ export const watch = (req, res) => {
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watcing ${video.title}`, video });
 };
-export const edit = (req, res) => res.render("edit", { pageTitle: "edit" });
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  res.render("edit", { pageTitle: `Editing ${video.title}`, video });
+};
+export const postEdit = () => {};
+
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("upload");
 export const deleteVideo = (req, res) => res.send("Delete Viedo");
