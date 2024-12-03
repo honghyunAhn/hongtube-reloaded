e23420f8b23c5b76d5dfa43a804e0c5356886a85
Ahn Honghyun <anh2073@naver.com>
2024-12-03 15:53:05 +0900

    Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -59,5 +59,9 @@ export const postUpload = async (req, res) => {
   }
 };
 
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
 export const search = (req, res) => res.send("Search");
-export const deleteVideo = (req, res) => res.send("Delete Viedo");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -12,8 +12,7 @@ const videoRouter = express.Router();
 
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-
-videoRouter.route("/:id(\\d+)/delete").get(deleteVideo);
 
 export default videoRouter;
