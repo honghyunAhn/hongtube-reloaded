e98961c6b43ea1614e74e44cc174c4b008258bd5
Ahn Honghyun <anh2073@naver.com>
2024-12-11 17:45:44 +0900

    Bugfix

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,7 +9,6 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner");
-  console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
@@ -18,9 +17,15 @@ export const watch = async (req, res) => {
 
 export const getEdit = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
   const video = await Video.findById(id);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   return res.render("videos/edit", {
     pageTitle: `Edit: ${video.title}`,
@@ -29,11 +34,17 @@ export const getEdit = async (req, res) => {
 };
 
 export const postEdit = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
   const video = await Video.exists({ _id: id });
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(video.owner) !== String(_id)) {
+    return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
     title,
@@ -75,6 +86,19 @@ export const postUpload = async (req, res) => {
 
 export const deleteVideo = async (req, res) => {
   const { id } = req.params;
+  const {
+    user: { _id },
+  } = req.session;
+  const video = await Video.findById(id).populate("owner");
+  const { _id: ownerId, videos } = video.owner;
+  if (!video) {
+    return res.status(404).render("404", { pageTitle: "Video not found." });
+  }
+  if (String(ownerId) !== String(_id)) {
+    return res.status(403).redirect("/");
+  }
+  videos.splice(videos.indexOf(id), 1);
+  await User.findByIdAndUpdate(ownerId, { videos });
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
 };
