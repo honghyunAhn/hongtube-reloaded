fc9e18bb224792a7197a6b1b70db0cdc609c59bc
Ahn Honghyun <anh2073@naver.com>
2024-12-11 12:52:01 +0900

    Video Upload

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -46,11 +46,13 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
+  const { title, description, hashtags } = req.body;
   try {
-    const { title, description, hashtags } = req.body;
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
