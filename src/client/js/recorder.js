f6a9c8c9418ec7aaedbce8209831069924c8e04e
Ahn Honghyun <anh2073@naver.com>
2024-12-18 14:17:00 +0900

    feat : Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,8 +1,32 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handelStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
@@ -10,4 +34,6 @@ const handelStart = async () => {
   video.play();
 };
 
-startBtn.addEventListener("click", handelStart);
+init();
+
+startBtn.addEventListener("click", handleStart);
