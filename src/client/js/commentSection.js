c0b4c0b48e4d5343a5983cfa95e133485b6167a4
Ahn Honghyun <anh2073@naver.com>
2024-12-25 16:47:43 +0900

    feat : API Route

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,13 +1,23 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
-  console.log(video);
+  const videoId = videoContainer.dataset.id;
+  if (text.trim() === "") {
+    return;
+  }
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
 };
 
-form.addEventListener("submit", handleSubmit);
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
