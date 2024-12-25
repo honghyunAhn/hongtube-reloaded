cc6cdf6d29a868f0f14cd7b4212c81bd49c1e659
Ahn Honghyun <anh2073@naver.com>
2024-12-25 14:22:47 +0900

    fix : Video edit error

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,12 +42,12 @@ export const postEdit = async (req, res) => {
   } = req.session;
   const { id } = req.params;
   const { title, description, hashtags } = req.body;
-  const video = await Video.exists({ _id: id });
+  const video = await Video.findById(id);
   if (!video) {
-    req.flash("error", "You are not the owner of the video.");
     return res.status(404).render("404", { pageTitle: "Video not found." });
   }
   if (String(video.owner) !== String(_id)) {
+    req.flash("error", "You are not the owner of the video.");
     return res.status(403).redirect("/");
   }
   await Video.findByIdAndUpdate(id, {
