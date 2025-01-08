676e7608d0f07ccf6f9a7f31c1ea2f15a6d6d3bf
Ahn Honghyun <anh2073@naver.com>
2025-01-08 23:47:10 +0900

    feat : Multer S3 Uploads

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,23 @@
 import multer from "multer";
+import { S3Client } from "@aws-sdk/client-s3";
+import multerS3 from "multer-s3";
+
+const s3Client = new S3Client({
+  region: "ap-northeast-2",
+  credentials: {
+    accessKeyId: process.env.AWS_KEY,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const s3AvatarStorage = multerS3({
+  s3: s3Client,
+  bucket: "hongtube-ahn",
+  acl: "public-read",
+  key: function (req, file, cb) {
+    cb(null, `avatars/${req.session.user._id}/${Date.now().toString()}`);
+  },
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -26,8 +45,8 @@ export const publicOnlyMiddleware = (req, res, next) => {
 };
 
 export const avatarUpload = multer({
-  dest: "uploads/avatars/",
   limits: { fileSize: 3000000 },
+  storage: s3AvatarStorage,
 });
 export const videoUpload = multer({
   dest: "uploads/videos/",
