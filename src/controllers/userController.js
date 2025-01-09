ff0c377eefc5be05a66b35d0a0808ea1dd17a1c4
Ahn Honghyun <anh2073@naver.com>
2025-01-10 00:19:57 +0900

    feat : Multer S3 Uploads_Update userController and VideoController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -176,7 +176,8 @@ export const postEdit = async (req, res) => {
   const updateUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.path : avatarUrl,
+      // avatarUrl: file ? file.path : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
